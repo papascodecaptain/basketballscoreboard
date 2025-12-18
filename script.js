2f529f614f9d2d6fb6f3fff4a444ee81a3a5b302
404 Pablo Not Found <62445045+papascodecaptain@users.noreply.github.com>
2025-12-17 18:14:41 -0800

    main functionality
    main JS functionality completed

---
+++ script.js
@@ -0,0 +1,53 @@
+
+let countEl = document.getElementById("score")
+let countEla = document.getElementById("scorea")
+let count = 0 
+
+function plus1(){
+    count += 1 
+    countEl.textContent = count
+}
+
+
+
+function plus2(){
+    count += 2 
+    countEl.textContent = count
+}
+
+
+function plus3(){
+    count += 3 
+    countEl.textContent = count
+}
+
+
+function reset(){
+    count = 0
+    countEl.textContent = count
+}
+
+
+function plus1a(){
+    count += 1 
+    countEla.textContent = count
+}
+
+
+
+function plus2a(){
+    count += 2 
+    countEla.textContent = count
+}
+
+
+function plus3a(){
+    count += 3 
+    countEla.textContent = count
+}
+
+
+function reseta(){
+    count = 0
+    countEla.textContent = count
+}
